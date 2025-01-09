9a1918024e362790520bc9e47194c850c1a90eeb
Daniel Garnier-Moiroux <git@garnier.wf>
2025-01-09 13:31:22 +0100

    schema.sql: remove unused DDL for login codes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -24,19 +24,3 @@ CREATE TABLE IF NOT EXISTS credentials_record
     created             TIMESTAMP
 );
 
-
---
--- CREATE TABLE IF NOT EXISTS "login_code"
--- (
---     id              UUID PRIMARY KEY NOT NULL,
---     user_id         UUID             NOT NULL REFERENCES app_user (id),
---     expiration_time TIMESTAMP DEFAULT (now() + INTERVAL '5 minutes')
--- );
---
--- CREATE TABLE IF NOT EXISTS "authenticator"
--- (
---     id                 TEXT PRIMARY KEY NOT NULL,
---     user_id            UUID             NOT NULL REFERENCES app_user (id),
---     credentials_name   TEXT             NOT NULL,
---     attestation_object BYTEA DEFAULT NULL
--- )
