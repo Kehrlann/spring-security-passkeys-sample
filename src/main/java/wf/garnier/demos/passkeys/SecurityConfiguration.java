21eb8a28c7de4919c1b6b6beffaea0972090f7b3
Daniel Garnier-Moiroux <git@garnier.wf>
2025-01-10 10:50:53 +0100

    add ott-login

--- src/main/java/wf/garnier/demos/passkeys/SecurityConfiguration.java
+++ src/main/java/wf/garnier/demos/passkeys/SecurityConfiguration.java
@@ -2,25 +2,40 @@ package wf.garnier.demos.passkeys;
 
 import javax.sql.DataSource;
 
+import wf.garnier.demos.passkeys.mail.MailNotifier;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.ott.OneTimeTokenGenerationSuccessHandler;
+import org.springframework.security.web.authentication.ott.RedirectOneTimeTokenGenerationSuccessHandler;
 
 @Configuration
 class SecurityConfiguration {
 
+	OneTimeTokenGenerationSuccessHandler ottSuccessHandler = new RedirectOneTimeTokenGenerationSuccessHandler(
+			"/login/ott");
+
 	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	SecurityFilterChain securityFilterChain(HttpSecurity http, MailNotifier mailNotifier) throws Exception {
 		//@formatter:off
 		return http
 				.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
 				.formLogin(login -> {
 					login.loginPage("/login");
 					login.permitAll();
-				}).webAuthn(passkeys -> {
+				})
+				.oneTimeTokenLogin(ott -> {
+					ott.tokenGenerationSuccessHandler((request, response, authentication) -> {
+						var token = authentication.getTokenValue();System.out.println("📩 Got token: " + token);
+						mailNotifier.notify("📩 Login", "You can log in with code " + token, "http://localhost:8080/login/ott?token=" + token);
+						ottSuccessHandler.handle(request, response, authentication);
+					});
+				})
+				.webAuthn(passkeys -> {
 					passkeys.rpName("Spring Security Relying Party");
 					passkeys.rpId("localhost");
 					passkeys.allowedOrigins("http://localhost:8080");
@@ -32,7 +47,7 @@ class SecurityConfiguration {
 
 	@Bean
 	UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+		return new JdbcUserDetailsManager(dataSource);
 	}
 
 }
