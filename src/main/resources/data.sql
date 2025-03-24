ffe4363892f647c08d6d8eabfab6dc208d21f66a
Daniel Garnier-Moiroux <git@garnier.wf>
2025-03-24 12:33:50 +0100

    fix: add anonymousUser to the database
    - This is a hack around https://github.com/spring-projects/spring-security/issues/16385

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,8 @@
 INSERT INTO users(username, password, enabled)
 VALUES ('daniel@example.com', '{bcrypt}$2y$10$7pZ0P3OYmahytG5jmedDH..X4Y7a5M50a4.7vTy0XoViM0KR0qVr6', true),
        ('alice@example.com', '{bcrypt}$2y$10$b2pljKWQhQEDH7eQxrr/mu728mGU2kJjRa2l.MIDPys92aqLYe92y', true),
-       ('bob@example.com', '{bcrypt}$2y$10$FLXrNbU589sOlM36DhhKQuNsfQheUW0cuJoaa5htN2tDrJdiFNtGe', true)
+       ('bob@example.com', '{bcrypt}$2y$10$FLXrNbU589sOlM36DhhKQuNsfQheUW0cuJoaa5htN2tDrJdiFNtGe', true),
+       ('anonymousUser', 'ignored', false)
 ON CONFLICT DO NOTHING;
 
 INSERT INTO authorities(username, authority)
