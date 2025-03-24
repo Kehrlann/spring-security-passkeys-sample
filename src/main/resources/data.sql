beb07350f2e410954eb62b6d45aa4f830713c3d3
Daniel Garnier-Moiroux <git@garnier.wf>
2025-03-24 12:33:22 +0100

    users: change usernames to ...@example.com

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
 -- All passwords are "password"
 INSERT INTO users(username, password, enabled)
-VALUES ('daniel', '{bcrypt}$2y$10$7pZ0P3OYmahytG5jmedDH..X4Y7a5M50a4.7vTy0XoViM0KR0qVr6', true),
-       ('alice', '{bcrypt}$2y$10$b2pljKWQhQEDH7eQxrr/mu728mGU2kJjRa2l.MIDPys92aqLYe92y', true),
-       ('bob', '{bcrypt}$2y$10$FLXrNbU589sOlM36DhhKQuNsfQheUW0cuJoaa5htN2tDrJdiFNtGe', true)
+VALUES ('daniel@example.com', '{bcrypt}$2y$10$7pZ0P3OYmahytG5jmedDH..X4Y7a5M50a4.7vTy0XoViM0KR0qVr6', true),
+       ('alice@example.com', '{bcrypt}$2y$10$b2pljKWQhQEDH7eQxrr/mu728mGU2kJjRa2l.MIDPys92aqLYe92y', true),
+       ('bob@example.com', '{bcrypt}$2y$10$FLXrNbU589sOlM36DhhKQuNsfQheUW0cuJoaa5htN2tDrJdiFNtGe', true)
 ON CONFLICT DO NOTHING;
 
 INSERT INTO authorities(username, authority)
-VALUES ('daniel', 'user'),
-       ('daniel', 'admin'),
-       ('alice', 'user'),
-       ('bob', 'user')
+VALUES ('daniel@example.com', 'user'),
+       ('daniel@example.com', 'admin'),
+       ('alice@example.com', 'user'),
+       ('bob@example.com', 'user')
 ON CONFLICT DO NOTHING;
