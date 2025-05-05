0f6bdb14cbdc8258f07b4ff796e23a35f2d5b410
Daniel Garnier-Moiroux <git@garnier.wf>
2025-05-05 09:23:54 +0200

    demo: bump to Boot 3.5.0-RC1 and JDK 24

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.0-M3"
+	id("org.springframework.boot") version "3.5.0-RC1"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -9,7 +9,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(23)
+		languageVersion = JavaLanguageVersion.of(24)
 	}
 }
 
