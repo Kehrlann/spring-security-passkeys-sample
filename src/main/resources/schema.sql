5dfb88fa712759a9be323c7af46b185f0af7169d
Daniel Garnier-Moiroux <git@garnier.wf>
2025-05-07 17:32:36 +0200

    demo: comment the schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,20 +16,31 @@ CREATE UNIQUE INDEX IF NOT EXISTS ix_auth_username ON authorities (username, aut
 -- Passkeys DDL
 CREATE TABLE IF NOT EXISTS user_credentials
 (
+    -- Primary key
     credential_id                text    not null PRIMARY KEY,
+
+    -- Foreign key
     user_entity_user_id          text    not null,
-    public_key                   bytea   not null,
+
+    -- All the data to perform passkeys signin
+    attestation_object           bytea,
+
+    -- Metadata
+    created                      timestamp,
+    last_used                    timestamp,
+    label                        text    not null,
+
+    -- Extra security feature
     signature_count              bigint,
+
+    -- What's this? What's this? There's data everywhere!
+    public_key                   bytea   not null,
     uv_initialized               boolean,
     backup_eligible              boolean not null,
     authenticator_transports     text,
     public_key_credential_type   text,
     backup_state                 boolean not null,
-    attestation_object           bytea,
-    attestation_client_data_json bytea,
-    created                      timestamp,
-    last_used                    timestamp,
-    label                        text    not null
+    attestation_client_data_json bytea
 );
 
 CREATE TABLE IF NOT EXISTS user_entities
