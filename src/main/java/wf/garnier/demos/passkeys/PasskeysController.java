eaa607c1c747c200dbb7a622f6ac09d0cae7c429
Daniel Garnier-Moiroux <git@garnier.wf>
2025-10-09 11:24:13 +0200

    demo: remove OTT

--- src/main/java/wf/garnier/demos/passkeys/PasskeysController.java
+++ src/main/java/wf/garnier/demos/passkeys/PasskeysController.java
@@ -1,12 +1,6 @@
 package wf.garnier.demos.passkeys;
 
-import java.util.List;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.webauthn.management.PublicKeyCredentialUserEntityRepository;
-import org.springframework.security.web.webauthn.management.UserCredentialRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
