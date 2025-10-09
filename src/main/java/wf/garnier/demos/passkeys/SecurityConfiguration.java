4b315f01891e45c46280249621ec46ceef3ad2b3
Daniel Garnier-Moiroux <git@garnier.wf>
2025-10-09 11:24:13 +0200

    demo: remove OTT

--- src/main/java/wf/garnier/demos/passkeys/SecurityConfiguration.java
+++ src/main/java/wf/garnier/demos/passkeys/SecurityConfiguration.java
@@ -4,21 +4,17 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.ott.OneTimeTokenGenerationSuccessHandler;
-import org.springframework.security.web.authentication.ott.RedirectOneTimeTokenGenerationSuccessHandler;
 
 @Configuration
 class SecurityConfiguration {
 
-	OneTimeTokenGenerationSuccessHandler DEFAULT_OTT_HANDLER = new RedirectOneTimeTokenGenerationSuccessHandler(
-			"/login/ott");
-
 	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http, OneTimeTokenMailNotifier mailNotifier) throws Exception {
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		//@formatter:off
 		return http
 				.authorizeHttpRequests(auth -> {
@@ -27,15 +23,7 @@ class SecurityConfiguration {
 					auth.requestMatchers("/error").permitAll();
 					auth.anyRequest().authenticated();
 				})
-                .oneTimeTokenLogin(ott -> {
-                    ott.tokenGenerationSuccessHandler((request, response, authentication) -> {
-                        var token = authentication.getTokenValue();
-                        var email = authentication.getUsername();
-                        mailNotifier.notify(email, "Demo app log in", token);
-                        System.out.println("📩 Got token: " + token);
-                        DEFAULT_OTT_HANDLER.handle(request, response, authentication);
-                    });
-                })
+                .formLogin(Customizer.withDefaults())
 				.logout(logout -> logout.logoutSuccessUrl("/login"))
 				.build();
 		//@formatter:on
