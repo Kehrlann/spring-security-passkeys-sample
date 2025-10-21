871693c9802e350870801724ffadddd951f1851a
Daniel Garnier-Moiroux <git@garnier.wf>
2025-10-21 15:33:31 +0200

    demo: use Boot 4.0.0-M3, Security 7.0.0-RC1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.5"
+	id("org.springframework.boot") version "4.0.0-M3"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -9,8 +9,11 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(24)
+		languageVersion = JavaLanguageVersion.of(25)
 	}
+}
+ext {
+	extra["spring-security.version"] = "7.0.0-RC1"
 }
 
 repositories {
@@ -24,7 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
-	implementation("com.webauthn4j:webauthn4j-core:0.29.6.RELEASE")
+	implementation("org.springframework.security:spring-security-webauthn")
 
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	implementation("org.springframework.boot:spring-boot-devtools")
