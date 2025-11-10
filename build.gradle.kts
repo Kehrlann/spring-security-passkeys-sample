957a89e5571947e29504739a3726880c1c546885
Daniel Garnier-Moiroux <git@garnier.wf>
2025-11-10 19:47:00 +0100

    demo: update Boot 4.0.0-M3 -> 4.0.0-RC2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "4.0.0-M3"
+	id("org.springframework.boot") version "4.0.0-RC2"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
